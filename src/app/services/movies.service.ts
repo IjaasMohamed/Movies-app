8f7260c53c9d492e64fcb1fbfa11ff734421b890
IjaasMohamed <undyin0@gmail.com>
2023-01-18 07:04:04 -0300

    First Commit

---
+++ src/app/services/movies.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MoviesService {
+  baseUrl: string = 'https://api.themoviedb.org/3';
+  apiKey: string = '9562123aafa5bae94fdf20ac66798ab6';
+
+  constructor(private http: HttpClient) {}
+
+  getMovies(type: string = 'upcoming') {
+    return this.http.get(`${this.baseUrl}/movie/${type}?api_key=${this.apiKey}`);
+  }
+}
