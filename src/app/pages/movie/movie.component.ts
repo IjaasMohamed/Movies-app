01b2e19ceac2f7a93074db09c9cb4ec21299ec18
IjaasMohamed <undyin0@gmail.com>
2023-01-29 22:58:28 -0300

    09-03

--- src/app/pages/movie/movie.component.ts
+++ src/app/pages/movie/movie.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Route } from '@angular/router';
 
 @Component({
   selector: 'app-movie',
   templateUrl: './movie.component.html',
   styleUrls: ['./movie.component.scss']
 })
-export class MovieComponent {
-
+export class MovieComponent implements OnInit {
+  constructor(private route: ActivatedRoute) {}
+  ngOnInit(): void {
+    this.route.params.subscribe(({ id }) => {
+      console.log(id);
+    });
+  }
 }
